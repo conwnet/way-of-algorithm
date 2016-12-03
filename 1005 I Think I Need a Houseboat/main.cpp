ad2e39b16fb9fd15a9c7b6d680f375aea9423257
netcon <netcon@live.com>
2016-12-03 16:59:24 +0800

    20161203

--- 1005 I Think I Need a Houseboat/main.cpp
+++ 1005 I Think I Need a Houseboat/main.cpp
@@ -8,7 +8,7 @@ int main()
     scanf("%d", &T);
     for(int nC = 1; nC <= T; nC++) {
         scanf("%lf%lf", &x, &y);
-        double s = (x * x + y * y) * 3.1415926535d;
+        double s = (x * x + y * y) * 3.1415926535;
         int ans = (s / 100) + 1;
         printf("Property %d: This property will begin eroding in year %d.\n", nC, ans);
     }
